37407d04672db7af30fa04a6757d3575b6ffd6a6
ichsanulamal <muhammad.ichsanul19@gmail.com>
2025-01-18 08:50:06 +0700

    p

--- example.sh
+++ example.sh
@@ -3,6 +3,7 @@
 # python vtt_dir2srt_dir.py "files" -r -e utf-8 
 python srt_dir2md.py "files" out.md
 python gptchunks.py
-find ./files -type f -name "*.txt" -exec rm -f {} \;
 
+find ./files -type f -name "*.srt" -exec rm -f {} \;
+find ./files -type f -name "*.txt" -exec rm -f {} \;
 find . -type f -name "chunk_*" -exec rm -f {} \;
